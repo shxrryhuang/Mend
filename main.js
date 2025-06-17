92896b1c540f529a9359587df6be4a61d6e4ce6b
Sherry <Shxrryhuang@gmail.com>
2025-06-17 16:02:33 -0400

    update packages for downloadingmendAI into app

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  // Loads the static HTML output from Next.js
+  win.loadFile(path.join(__dirname, 'out/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
