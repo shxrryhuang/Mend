047096ea07b5d1d4d93933dfa5a30d84fe2e329e
Sherry <Shxrryhuang@gmail.com>
2025-08-28 04:16:54 -0400

    remove node_modules from repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-    output: 'export',
-};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',  // 👈 tells Next.js to build as static
+    images: {
+      unoptimized: true, // needed for GitHub Pages since Next Image optimizer won’t work
+    },
+  };
+  
+  module.exports = nextConfig;
+  
